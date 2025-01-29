bed8efa43b1aa22c8aa6d37b57855159f579c267
amit <amitmohammad46@gmail.com>
2025-01-30 00:43:43 +0600

    add category

--- src/Components/ShopSidebar.jsx
+++ src/Components/ShopSidebar.jsx
@@ -3,7 +3,37 @@
 const ShopSidebar = () => {
   return (
     <div className="w-3/12 p-3">
-      sidebar
+      <h3 className="text-2xl font-brand text-slate-900 font-[600]">Category</h3>
+      <div className="flex flex-col gap-3">
+        <div className="transition flex items-center justify-between border border-[#ececec] rounded hover:shadow-new hover:border-[#BCE3C9] p-2 ">
+          <span className="text-base font-brand text-gray-800 font-[500]">Category</span>
+          <span className="h-[28px] w-[28px] p-1 rounded-full bg-[#3BB77E] text-white flex items-center justify-center font-brand text-[12px]">32</span>
+        </div>
+        <div className="transition flex items-center justify-between border border-[#ececec] rounded hover:shadow-new hover:border-[#BCE3C9] p-2 ">
+          <span className="text-base font-brand text-gray-800 font-[500]">Category</span>
+          <span className="h-[28px] w-[28px] p-1 rounded-full bg-[#3BB77E] text-white flex items-center justify-center font-brand text-[12px]">32</span>
+        </div>
+        <div className="transition flex items-center justify-between border border-[#ececec] rounded hover:shadow-new hover:border-[#BCE3C9] p-2 ">
+          <span className="text-base font-brand text-gray-800 font-[500]">Category</span>
+          <span className="h-[28px] w-[28px] p-1 rounded-full bg-[#3BB77E] text-white flex items-center justify-center font-brand text-[12px]">32</span>
+        </div>
+        <div className="transition flex items-center justify-between border border-[#ececec] rounded hover:shadow-new hover:border-[#BCE3C9] p-2 ">
+          <span className="text-base font-brand text-gray-800 font-[500]">Category</span>
+          <span className="h-[28px] w-[28px] p-1 rounded-full bg-[#3BB77E] text-white flex items-center justify-center font-brand text-[12px]">32</span>
+        </div>
+        <div className="transition flex items-center justify-between border border-[#ececec] rounded hover:shadow-new hover:border-[#BCE3C9] p-2 ">
+          <span className="text-base font-brand text-gray-800 font-[500]">Category</span>
+          <span className=" p-1 rounded-full bg-[#3BB77E] text-white flex items-center justify-center font-brand text-[12px]">32</span>
+        </div>
+        <div className="transition flex items-center justify-between border border-[#ececec] rounded hover:shadow-new hover:border-[#BCE3C9] p-2 ">
+          <span className="text-base font-brand text-gray-800 font-[500]">Category</span>
+          <span className="h-[28px] w-[28px] p-1 rounded-full bg-[#3BB77E] text-white flex items-center justify-center font-brand text-[12px]">32</span>
+        </div>
+        <div className="transition flex items-center justify-between border border-[#ececec] rounded hover:shadow-new hover:border-[#BCE3C9] p-2 ">
+          <span className="text-base font-brand text-gray-800 font-[500]">Category</span>
+          <span className="h-[28px] w-[28px] p-1 rounded-full bg-[#3BB77E] text-white flex items-center justify-center font-brand text-[12px]">32</span>
+        </div>
+      </div>
     </div>
   );
 };
