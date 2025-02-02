8a17a23bc2f702fb9426199d496f621e5aa8818f
“Amit <amitmohammad46@gmail.com>
2025-02-02 14:05:18 +0600

    footer design

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router";
 
 const Footer = () => {
   return (
-    <div className="bg-slate-500 py-3">
-      <h2 className="text-3xl font-semibold text-white text-center">Footer</h2>
+    <div className="bg-slate-900 py-2">
+      <div className="fashion-container">
+        <p className="text-sm font-inter text-slate-200 text-center mb-0">© Copyright 2025 All Rights Reserved</p>
+        <p className="text-sm font-inter text-slate-200 text-center mb-0">Developed by <Link to="/">Amit</Link></p>
+      </div>
     </div>
   );
 };
