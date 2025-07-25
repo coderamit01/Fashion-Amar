64a9fb47ef4b3ea5f1fea4f281d555c657795aa4
amit <amitmohammad46@gmail.com>
2025-07-25 23:56:15 +0600

    added product categories in shop page sidebar

--- src/Components/ProductSidebar/ProductSidebar.jsx
+++ src/Components/ProductSidebar/ProductSidebar.jsx
@@ -1,16 +1,60 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const ProductSidebar = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://dummyjson.com/products/category-list"
+      );
+      const data = await response.data;
+      setCategories(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="col-span-12 lg:col-span-3">
-      <h5 className="text-xl font-semibold text-stone-950">Category</h5>
-      <div className="flex flex-col space-y-1.5 pt-2">
-        <div className="flex items-center">
-          <input type="checkbox" name="" id="11" className="me-1.5 accent-brand h-4 w-4" />
-          <label htmlFor="11">
-            <span className="text-base capitalize text-slate-800 font-brand font-semibold">delta</span>
-            {/* <span className="before:absolute before:h-[18px] before:w-[18px] before:border before:border-slate-400 before:-left-[25px] before:top-[50%] before:-translate-y-[50%]  ms-[25px] relative">sdlkf</span> */}
-          </label>
+      <div className="pb-3">
+        <h5 className="text-xl font-semibold text-stone-950">Category</h5>
+        <div className="flex flex-col space-y-1.5 pt-2">
+          {categories.map((category, idx) => (
+            <div className="flex items-center" key={idx}>
+              <input
+                type="checkbox"
+                name=""
+                id={category}
+                className="me-1.5 accent-brand h-4 w-4"
+              />
+              <label htmlFor={category}>
+                <span className="text-base capitalize text-slate-800 font-brand font-semibold">
+                  {category}
+                </span>
+              </label>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="pb-3">
+        <h5 className="text-xl font-semibold text-stone-950">Brand</h5>
+        <div className="flex flex-col space-y-1.5 pt-2">
+          {categories.map((category, idx) => (
+            <div className="flex items-center" key={idx}>
+              <input
+                type="checkbox"
+                name=""
+                id={category}
+                className="me-1.5 accent-brand h-4 w-4"
+              />
+              <label htmlFor={category}>
+                <span className="text-base capitalize text-slate-800 font-brand font-semibold">
+                  {category}
+                </span>
+              </label>
+            </div>
+          ))}
         </div>
       </div>
     </div>
