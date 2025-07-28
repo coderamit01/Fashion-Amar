032f5dac7bde48c852dba3a1d34f9cfe9433f681
amit <amitmohammad46@gmail.com>
2025-07-29 00:59:31 +0600

    search functionality

--- src/Components/ArchiveProducts/ArchiveProducts.jsx
+++ src/Components/ArchiveProducts/ArchiveProducts.jsx
@@ -1,35 +1,21 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import ProductCard from "../Products/ProductCard";
 import Loader from "../Loader/Loader";
 
-const ArchiveProducts = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
+const ArchiveProducts = ({loading,products,setSearchText}) => {
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("https://dummyjson.com/products");
-        const data = await response.data;        
-        setProducts(data.products);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
   if (loading) return (
     <div className="col-span-12 lg:col-span-9">{<Loader />}</div>
   );
   return (
     <div className="col-span-12 lg:col-span-9">
-      <div className="grid md:grid-cols-3 lg:grid-cols-4 grid-cols-2 gap-5 ">
+      <input type="text" name="" id="" onChange={(e) => setSearchText(e.target.value)} className="border border-slate-300 outline-0 px-2 py-1 rounded-[3px] w-full" placeholder="Search..." />
+      <div className="grid md:grid-cols-3 lg:grid-cols-4 grid-cols-2 gap-5 pt-4">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+      
+          {console.log(products)}
     </div>
   );
 };
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,47 @@
+import { useEffect, useState } from "react";
 import ArchiveProducts from "../Components/ArchiveProducts/ArchiveProducts";
 import ProductSidebar from "../Components/ProductSidebar/ProductSidebar";
+import axios from "axios";
 
 const Shop = () => {
+  
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://dummyjson.com/products");
+        const data = await response.data;
+        setProducts(data.products);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const getFilterProducts = () => {
+    let filtered = products;
+    if (searchText) {
+      filtered = filtered.filter(
+        (product) =>
+          product.title.toLowerCase().includes(searchText.toLowerCase()) ||
+          product.brand?.toLowerCase().includes(searchText.toLowerCase())
+      );
+    }
+
+    return filtered;
+  }
+  const filterProducts = getFilterProducts();
+
   return (
     <div className="fashion-container">
       <div className="grid grid-cols-12  gap-5 py-10">
         <ProductSidebar />
-        <ArchiveProducts />
+        <ArchiveProducts loading={loading} products={filterProducts} setSearchText={setSearchText} />
       </div>
     </div>
   );
