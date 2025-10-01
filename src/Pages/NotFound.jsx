1fedb5eff34474ce85e644c680dec1086b8c544a
amit <amitmohammad46@gmail.com>
2025-10-02 00:04:03 +0600

    redesign 404 page

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,11 +1,13 @@
 import { NavLink } from "react-router";
+import NotFoundImg from "../../public/assets/images/404.png";
 
 const NotFound = () => {
   return (
     <div className="w-full grid items-center justify-center">
-      <div>
-        <h2 className="text-center text-5xl font-bold">404 Not Found</h2>
-        <div className="text-center pt-10">
+      <div className="py-8">
+        <img src={NotFoundImg} alt="404 Image" />
+        <div className="text-center space-y-3">
+        <p className="text-base font-brand">Sorry, the page you visited does not exist.</p>
           <NavLink to="/" className="py-2 px-5 bg-brand text-white rounded">
             Go to Home
           </NavLink>
